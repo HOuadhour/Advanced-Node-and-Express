685f5350018b0b73214531a7913c161a374545a0
HOuadhour <HOuadhour@Yandex.com>
2021-03-17 15:14:52 +0100

    Set up Passport.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
 
 const app = express();
 app.set("view engine", "pug");
@@ -13,6 +15,18 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   res.render(path.join(__dirname, "views/pug"), {
