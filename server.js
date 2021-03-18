057c3d70ca6ea8e1d02a3efca477b57b983ace1b
HOuadhour <HOuadhour@Yandex.com>
2021-03-18 18:23:53 +0100

    Implement the Serialization of a Passport User.

--- server.js
+++ server.js
@@ -30,21 +30,32 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+myDB(async client => {
+  const myDataBase = await client.db("database").collection("users");
 
-passport.deserializeUser((id, done) => {
-  done(null, null);
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  //   done(null, null);
-  // });
-});
+  // Be sure to change the title
+  app.route("/").get((req, res) => {
+    //Change the response to render the Pug template
+    res.render("pug", {
+      title: "Connected to Database",
+      message: "Please login",
+    });
+  });
 
-app.route("/").get((req, res) => {
-  res.render(path.join(__dirname, "views/pug"), {
-    title: "Hello",
-    message: "Please login",
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
   });
 });
 
